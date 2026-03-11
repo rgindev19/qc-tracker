baee418f858c80548098d285fb616ca8f1004375
rgindev19 <rginmagdasocsasis@gmail.com>
2026-03-11 22:51:39 +0800

    Finalized PWA offline queue and fixed TS errors

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,9 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "@/app/(app)/globals.css";
 import { cn } from "@/lib/utils";
+import QueueStatus from "./QueueStatus";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
@@ -27,12 +29,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", figtree.variable)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <html lang="en">
+  <body>
+    {/* Now it is safely inside the body where a <div> is allowed */}
+    <QueueStatus />
+    {children}
+  </body>
+</html>
   );
 }
